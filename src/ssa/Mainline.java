a9d0a2bb428ea301c49581f06dc3751f57843657
Kyle <kcd5059@gmail.com>
2016-09-16 16:51:33 -0400

    Updates

--- src/ssa/Mainline.java
+++ src/ssa/Mainline.java
@@ -6,7 +6,6 @@ public class Mainline {
 
 		Savings savings = new Savings(500);
 		savings.setInterestRate(1.5);
-		savings.setMinimumBalance(100);
 		System.out.println(savings.print());
 		savings.calcDepositInterest(4);
 		System.out.println(savings.print());
